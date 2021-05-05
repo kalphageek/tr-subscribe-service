ac4baa044c7302ffa7dbd41c32d933b68004f28f
kalphageek <kalphageek@outlook.com>
2021-05-05 11:35:57 +0900

    first commit

---
+++ src/main/java/me/kalpha/trsubscribeservice/TrSubscribeServiceApplication.java
@@ -0,0 +1,13 @@
+package me.kalpha.trsubscribeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrSubscribeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrSubscribeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/kalpha/trsubscribeservice/TrSubscribeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kalpha.trsubscribeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrSubscribeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
