1dc7b0b1cbe92096ddd85ee84025b03aa6d22e1a
kalphageek <kalphageek@outlook.com>
2021-05-07 21:37:35 +0900

    service-start.sh 수정

--- service-start.sh
+++ service-start.sh
@@ -3,6 +3,8 @@
 #Set at jenkins executor
 appName=$1
 
+WHOAMI=$(whoami)
+echo "> whoami : $WHOAMI"
 cd ~/workspace/
 DIR=$(ls | grep $appName | head -n 1)
 cd $DIR
@@ -15,7 +17,7 @@ then
   echo "> there is no running process."
 else
   echo "> kill -9 $CURRENT_PID"
-  kill -9 $CURRENT_PID sleep 10
+  kill -9 $CURRENT_PID | sleep 10
 fi
 echo "> deploy new version."
 nohup java -jar $JAR_NAME &
