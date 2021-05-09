1af95ea69f2e9a75f2b948241c8e082e57c774e0
kalphageek <kalphageek@outlook.com>
2021-05-09 22:34:41 +0900

    temp added 2 files delete

--- src/main/java/me/kalpha/trsubscribeservice/trmart/service/Eqp1TrConsumerService.java
+++ src/main/java/me/kalpha/trsubscribeservice/trmart/service/Eqp1TrConsumerService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import me.kalpha.trsubscribeservice.trmart.entity.Eqp1Tr;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -21,10 +22,12 @@ public class Eqp1TrConsumerService {
     public void receiveMessage(@Payload Eqp1Tr eqp1Tr,
                                @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
                                @Header(KafkaHeaders.RECEIVED_PARTITION_ID) Integer partition,
-                               @Header(KafkaHeaders.OFFSET) Long offset) {
+                               @Header(KafkaHeaders.OFFSET) Long offset,
+                               @Header(KafkaHeaders.ACKNOWLEDGMENT)Acknowledgment acknowledgment) {
         log.info("Received message: data = {}, topic = {}, partition = {}, offset = {}", eqp1Tr, topic, partition, offset);
 
         eqp1TrService.createTr(eqp1Tr);
+        acknowledgment.acknowledge();
     }
 //    @KafkaListener(topics = "${app.topic.name}", groupId = "${spring.kafka.consumer.group-id}", containerFactory = "eqp1TrKafkaListenerContainerFactory")
 //    public void onMessage(ConsumerRecords<String, Eqp1Tr> consumerRecords) {
